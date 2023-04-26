74121146ea1e811645894181b669e86ad201f271
WsPrj <wernisawruersp@gmail.com>
2023-04-26 17:06:39 -0300

    Clase5

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -2,15 +2,23 @@
 
 namespace MvcMovie.Controllers
 {
-    /*public class HelloWorldController : Controller
+    public class HelloWorldController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-    }*/
-    /*
-    public string Index()
+
+        /*public IActionResult Welcome(String name, int numTimes = 1)
+        {
+            ViewData["name"] = name;
+            ViewData["number"] = numTimes;
+
+            return View();
+        }*/
+    }
+    
+    /*public string Index()
     {
         return "This is my default action...";
     }
---
+++ MvcMovie/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MovieController : Controller
+    {
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+            return View(movie);
+        }
+    }
+}
